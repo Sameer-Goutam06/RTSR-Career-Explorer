dd649d90861a23a74cde6d6bb2428667264c7df9
Sameer-Goutam06 <r.n.v.s.sgautham@gmail.com>
2024-06-14 12:48:54 +0530

    Created new routes and templates

--- index.js
+++ index.js
@@ -7,20 +7,62 @@ const port=8080;
 //we need to create a folder nameed public to serve static files such as html,css,ejs..
 app.use(express.static(path.join(__dirname,"/public/CSS")));
 app.use(express.static(path.join(__dirname,"/public/JS")));
+
 //using of ejs which is already installed in express folder hence we need to use it using app.set()
 //view => templates
 app.set("view engine", "ejs");
+
 //setting path to run server globally and to avoid views loading from root directory when it is located in a leaf directory
 app.set("views",path.join(__dirname,"/views"));
+
+//to get the data from the post method
+app.use(express.urlencoded({extended:true}));
+
 app.listen(port,()=>{
     console.log(`listening on port ${port}`);
 });
 
-//for a basic response
-// app.get('/', function(req, res){
-//     res.send("Welcome home");
-// });
+//home route
 app.get("/",(req,res)=>
 {
-    res.render("home.ejs");
-})
+    res.render("home.ejs",{port});
+});
+
+//careers route
+app.get("/careers",(req,res)=>{
+    res.render("careers.ejs",{port});
+});
+
+//Assessment Route
+app.get("/assessment",(req,res)=>{
+    res.render("assessment.ejs",{port});
+});
+
+//Login Route get request
+app.get("/login",(req,res)=>{
+    res.render("login.ejs",{port});
+});
+
+//Login Route post request to get data and check if user exists
+app.post("/login",(req,res)=>
+{
+    console.log(req.body);
+});
+
+//registration route
+app.get("/register",(req,res)=>{
+    res.render("register.ejs",{port});
+});
+
+//registration route to check if user exists before
+app.post("/register",(req,res)=>
+{
+    let {username,email,password,confirmPassword}=req.body;
+    if (password !== confirmPassword) {
+        return res.render('error.ejs', { data: 'Passwords do not match' });
+    }
+    else
+    {
+        
+    }
+});
